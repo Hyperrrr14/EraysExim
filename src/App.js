4c95b18fc928efba8beb7a484edebc3348512b44
Hyperrrr14 <arashid@innovationteam.com>
2025-10-22 20:04:41 +0530

    modify the homepage url

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Navbar />
       <div className="content">
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/product" element={<Product />} />
           <Route path="/contact" element={<Contact />} />
